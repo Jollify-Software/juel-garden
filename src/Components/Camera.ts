ddc392a444ce3777ef137026299edd495995da06
Luke <obrienluk89@gmail.com>
2022-04-10 17:51:24 +0100

    JoystickCamera

--- src/Components/Camera.ts
+++ src/Components/Camera.ts
@@ -1,4 +1,4 @@
-import { ArcRotateCamera, Camera, FreeCamera, Vector3 } from "babylonjs";
+import { ArcRotateCamera, Camera, FreeCamera, Vector3, VirtualJoystick, VirtualJoysticksCamera } from "babylonjs";
 import { customElement, property } from "lit/decorators";
 import { Vector3Convert } from "../Converters/Vector3Convert";
 import { GardenElement } from "../GardenElement";
@@ -10,6 +10,7 @@ export class GardenCamera extends GardenElement {
     @property({ type: Number }) speed: number;
     @property({ converter: Vector3Convert.fromString }) position: Vector3;
     @property({ converter: Vector3Convert.fromString }) ellipsoid: Vector3;
+    @property() touch: string;
 
     camera: Camera;
 
@@ -18,6 +19,7 @@ export class GardenCamera extends GardenElement {
         this.type = 'arc';
         this.speed = 0.4;
         this.ellipsoid = new Vector3(1, 1, 1);
+        this.touch = 'true';
     }
 
     getPosition() {
@@ -30,24 +32,33 @@ export class GardenCamera extends GardenElement {
     updated() {
         let sceneEl = this.parentElement as GardenScene
         let scene = sceneEl.scene
-console.log(this.type)
+        console.log(this.type)
         switch (this.type) {
             case 'arc':
-                this.camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new Vector3(0, 0, 0), scene);      
+                this.camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new Vector3(0, 0, 0), scene);
                 break;
             case 'free':
-                let cam = new FreeCamera("camera", new Vector3(0, 5, -10), scene);
+                let cam: FreeCamera;
+                const userAgent = navigator.userAgent.toLowerCase();
+                const isTablet = /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(userAgent);
+                if (this.touch == 'true' && isTablet && ('ontouchstart' in window || navigator.maxTouchPoints > 0)) {
+                    cam = new VirtualJoysticksCamera("camera", new Vector3(0, 5, -10), scene);
+                } else {
+                    cam = new FreeCamera("camera", new Vector3(0, 5, -10), scene);
+                }
                 if (this.ellipsoid)
                     cam.ellipsoid = this.ellipsoid;
-                    
-        if (this.hasAttribute("collisions")) {
-            cam.checkCollisions = true;
-            cam.applyGravity = true;
-        }
-        if (this.speed)
-            cam.speed = this.speed;
-            
-            this.camera = cam;
+
+                if (this.hasAttribute("collisions")) {
+                    cam.checkCollisions = true;
+                    cam.applyGravity = true;
+                }
+                if (this.speed)
+                    cam.speed = this.speed;
+    
+                this.camera = cam;
+                
+                break;
             default:
                 break;
         }
@@ -55,10 +66,10 @@ console.log(this.type)
         if (this.position)
             this.camera.position = this.position;
 
-            let opt = this.buildOptions();
+        let opt = this.buildOptions();
         this.camera = Object.assign(this.camera, opt);
 
-this.camera.attachControl(sceneEl.canvas, true);
+        this.camera.attachControl(sceneEl.canvas, true);
 
     }
 }
